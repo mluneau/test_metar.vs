dec7f67ff6f7577235d935449575f051aa6db562
Maxence Luneau <maxenceluneau@gmail.com>
2022-08-23 19:02:27 +0200

    Test technique for Metav.rs init commit

---
+++ contracts.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.17;
+
+/* 
+
+Technical test for Meta.vs
+
+Challenge:
+1. Create a contract file .sol in about 2h.
+2. It should distrube up to 100 NFT drops (maximum).
+3. Mint fee is to be paid by the person acquiring the NFT.
+4. To mint the NFT, the user must proceed through a web page and with a click action.
+
+*/
+
+contract Nft_creation {
+  
+}
